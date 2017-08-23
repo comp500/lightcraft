83a04fea1bc338c511d69f7cf1d8cc88be6a4ca0
comp500 <comp500@users.noreply.github.com>
2017-08-23 12:39:08 +0100

    Add basic polisher texture

--- src/main/java/link/infra/lightcraft/blocks/Polisher.java
+++ src/main/java/link/infra/lightcraft/blocks/Polisher.java
@@ -3,11 +3,21 @@ package link.infra.lightcraft.blocks;
 import link.infra.lightcraft.LightCraft;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class Polisher extends Block {
     public Polisher() {
         super(Material.ROCK);
         setUnlocalizedName(LightCraft.MODID + ".polisher");     // Used for localization (en_US.lang)
         setRegistryName("polisher");        // The unique name (within your mod) that identifies this block
+    }
+    
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
     }
 }
