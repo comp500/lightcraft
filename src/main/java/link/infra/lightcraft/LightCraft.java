35451cc84bc2b7610eb9a371093356ea6084d28d
comp500 <comp500@users.noreply.github.com>
2017-08-23 13:48:59 +0100

    bump version

--- src/main/java/link/infra/lightcraft/LightCraft.java
+++ src/main/java/link/infra/lightcraft/LightCraft.java
@@ -12,7 +12,7 @@ public class LightCraft {
 
     public static final String MODID = "lightcraft";
     public static final String MODNAME = "LightCraft";
-    public static final String VERSION = "1.0.0.1";
+    public static final String VERSION = "1.0.0.2";
 
     @SidedProxy(clientSide = "link.infra.lightcraft.proxy.ClientProxy", serverSide = "link.infra.lightcraft.proxy.CommonProxy")
     public static CommonProxy proxy;
