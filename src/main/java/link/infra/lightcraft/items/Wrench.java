a274634b7d663f13fdeb0cf840f2da2130346c41
comp500 <comp500@users.noreply.github.com>
2017-08-23 20:58:04 +0100

    make wrench only stackable to 1 and add recipe

--- src/main/java/link/infra/lightcraft/items/Wrench.java
+++ src/main/java/link/infra/lightcraft/items/Wrench.java
@@ -3,6 +3,7 @@ package link.infra.lightcraft.items;
 import link.infra.lightcraft.LightCraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -18,4 +19,8 @@ public class Wrench extends Item {
     public void initModel() {
         ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
     }
+	
+	public int getItemStackLimit(ItemStack i) {
+		return 1;
+	}
 }
