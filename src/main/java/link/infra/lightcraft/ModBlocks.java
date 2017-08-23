10b02cc48d01572d6b1823763cb13dbfb6e7b086
comp500 <comp500@users.noreply.github.com>
2017-08-23 12:46:23 +0100

    register models

--- src/main/java/link/infra/lightcraft/ModBlocks.java
+++ src/main/java/link/infra/lightcraft/ModBlocks.java
@@ -2,9 +2,16 @@ package link.infra.lightcraft;
 
 import link.infra.lightcraft.blocks.*;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ModBlocks {
 
     @GameRegistry.ObjectHolder("lightcraft:polisher")
     public static Polisher polisher;
+    
+    @SideOnly(Side.CLIENT)
+    public static void initModels() {
+        polisher.initModel();
+    }
 }
