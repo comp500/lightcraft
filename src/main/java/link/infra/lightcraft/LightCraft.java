747b4dc388b671617147a74d609e6b959fb4efe9
comp500 <comp500@users.noreply.github.com>
2017-08-23 12:27:31 +0100

    fix proxy names

--- src/main/java/link/infra/lightcraft/LightCraft.java
+++ src/main/java/link/infra/lightcraft/LightCraft.java
@@ -7,14 +7,14 @@ import net.minecraftforge.fml.common.event.*;
 
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = LightCraft.MODID, name = LightCraft.MODNAME, version = LightCraft.VERSION, dependencies = "required-after:Forge@[14.21.0.2320,)", useMetadata = true)
+@Mod(modid = LightCraft.MODID, name = LightCraft.MODNAME, version = LightCraft.VERSION, useMetadata = true)
 public class LightCraft {
 
     public static final String MODID = "lightcraft";
     public static final String MODNAME = "LightCraft";
     public static final String VERSION = "1.0.0";
 
-    @SidedProxy(clientSide = "mcjty.modtut.proxy.ClientProxy", serverSide = "mcjty.modtut.proxy.ServerProxy")
+    @SidedProxy(clientSide = "link.infra.lightcraft.proxy.ClientProxy", serverSide = "link.infra.lightcraft.proxy.CommonProxy")
     public static CommonProxy proxy;
 
     @Mod.Instance
