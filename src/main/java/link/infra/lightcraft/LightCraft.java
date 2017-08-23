820d4d3de67417401438cb7fb54d8d53265b0352
comp500 <comp500@users.noreply.github.com>
2017-08-23 12:07:07 +0100

    Add main classes

---
+++ src/main/java/link/infra/lightcraft/LightCraft.java
@@ -0,0 +1,40 @@
+package link.infra.lightcraft;
+
+import link.infra.lightcraft.proxy.CommonProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.*;
+
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = LightCraft.MODID, name = LightCraft.MODNAME, version = LightCraft.VERSION, dependencies = "required-after:Forge@[11.16.0.1865,)", useMetadata = true)
+public class LightCraft {
+
+    public static final String MODID = "lightcraft";
+    public static final String MODNAME = "LightCraft";
+    public static final String VERSION = "1.0.0";
+
+    @SidedProxy(clientSide = "mcjty.modtut.proxy.ClientProxy", serverSide = "mcjty.modtut.proxy.ServerProxy")
+    public static CommonProxy proxy;
+
+    @Mod.Instance
+    public static LightCraft instance;
+
+    public static Logger logger;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+        proxy.preInit(event);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent e) {
+        proxy.init(e);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent e) {
+        proxy.postInit(e);
+    }
+}
---
+++ src/main/java/link/infra/lightcraft/proxy/CommonProxy.java
@@ -0,0 +1,28 @@
+package link.infra.lightcraft.proxy;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.*;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber
+public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent e) {
+    }
+
+    public void init(FMLInitializationEvent e) {
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+    }
+
+    @SubscribeEvent
+    public static void registerBlocks(RegistryEvent.Register<Block> event) {
+    }
+
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event) {
+    }
+}
