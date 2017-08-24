1998d8c5d6c97f0ef39d047dcc4a5344bc41d960
comp500 <comp500@users.noreply.github.com>
2017-08-24 17:47:13 +0100

    add JEI api

---
+++ src/main/java/link/infra/lightcraft/jei/JeiPlugin.java
@@ -0,0 +1,15 @@
+package link.infra.lightcraft.jei;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.JEIPlugin;
+
+@JEIPlugin
+public class JeiPlugin implements IModPlugin {
+
+	@Override
+	public void register(IModRegistry registry) {
+		
+	}
+	
+}
